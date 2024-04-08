c462f5e959fe28959e220d8e75f60c61e7d703f3
TunaSandwich <davide.faggionato31@gmail.com>
2024-04-08 12:19:47 +0200

    Feat: implemented project base

--- bw3-crud/README.md
+++ bw3-crud/README.md
@@ -1,4 +1,4 @@
-# Bw3CrudApp
+# Bw3Crud
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.12.
 
--- bw3-crud/src/app/app.component.spec.ts
+++ bw3-crud/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bw3_crudApp'`, () => {
+  it(`should have as title 'bw3-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bw3_crudApp');
+    expect(app.title).toEqual('bw3-crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bw3_crudApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bw3-crud app is running!');
   });
 });
