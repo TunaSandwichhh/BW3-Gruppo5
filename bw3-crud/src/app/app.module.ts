896bf4543422d34d55bc5da16a8d7e732eaab6a8
TunaSandwich <davide.faggionato31@gmail.com>
2024-04-08 13:11:11 +0200

    Feat: creato cartelle components + db.json

--- bw3-crud/src/app/app.module.ts
+++ bw3-crud/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsPageComponent } from './components/posts-page/posts-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    PostsPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
