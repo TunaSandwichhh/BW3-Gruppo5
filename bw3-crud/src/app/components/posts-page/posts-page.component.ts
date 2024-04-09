17acc25fcdc4ac51c2f17b8bf252817fc67c0a9e
fdinocera <fdinocera@libero.it>
2024-04-09 10:54:12 +0200

    feat: implementazione postPageComponent

--- bw3-crud/src/app/components/posts-page/posts-page.component.ts
+++ bw3-crud/src/app/components/posts-page/posts-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostInterface } from 'src/app/models/post-interface';
 import { PostsServiceService } from 'src/services/posts-service.service';
 
 @Component({
@@ -6,12 +7,15 @@ import { PostsServiceService } from 'src/services/posts-service.service';
   templateUrl: './posts-page.component.html',
   styleUrls: ['./posts-page.component.scss']
 })
-export class PostsPageComponent implements OnInit{
+export class PostsPageComponent implements OnInit {
 
-  constructor(private postService: PostsServiceService){}
+  posts!: PostInterface[];
+
+  constructor(private postService: PostsServiceService) { }
 
   ngOnInit(): void {
-
+    this.postService.getPosts().subscribe(data => {
+      this.posts = data;
+    })
   }
-
 }
--- bw3-crud/src/services/posts-service.service.ts
+++ bw3-crud/src/services/posts-service.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostInterface } from 'src/app/models/post-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsServiceService {
 
-
   apiURL = 'http://localhost:3000/';
   constructor(private http: HttpClient) { }
 
-  getPosts() {
-    return this.http.get<any>(this.apiURL + 'posts');
+  getPosts(): Observable<PostInterface[]> {
+    return this.http.get<PostInterface[]>(this.apiURL + 'posts');
   }
 }
