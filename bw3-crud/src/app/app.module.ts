3a36c9585189ca5475388b40a735b2c4b6f11f76
TunaSandwich <davide.faggionato31@gmail.com>
2024-04-09 10:59:17 +0200

    Update: updated imports ngModule

--- bw3-crud/src/app/app.module.ts
+++ bw3-crud/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,7 @@ import { ErrorPageComponent } from './components/error-page/error-page.component
     EditPostPageComponent,
     ErrorPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
