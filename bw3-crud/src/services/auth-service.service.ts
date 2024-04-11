fb873fdee2f84cefe81ba846d9f1c073f1d4cccc
TunaSandwich <davide.faggionato31@gmail.com>
2024-04-11 11:17:22 +0200

    Feat: implemented signup funtionality

--- bw3-crud/src/services/auth-service.service.ts
+++ bw3-crud/src/services/auth-service.service.ts
@@ -1,8 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserSession } from 'src/app/models/UserSession';
+import { SignupRequestBody } from 'src/app/models/signup-request-body.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthServiceService {
-  constructor() {}
+  isLoggedIn = false;
+
+  signupUrl =
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDQg7VJW4VX849JdUjwRYaxwMNP5CVHf1I';
+
+  loginUrl =
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDQg7VJW4VX849JdUjwRYaxwMNP5CVHf1I';
+
+  userSession!: UserSession | null;
+
+  constructor(private http: HttpClient) {}
+
+  isAuthenticated() {
+    return this.isLoggedIn;
+  }
+
+  signup(body: SignupRequestBody) {
+    return this.http.post(this.signupUrl, body);
+  }
 }
